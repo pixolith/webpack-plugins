89c9642fa1fe7439c0fe3a4e6fc7cf8020459f31
Simon Kunz <simon.kunz@pixolith.de>
2020-02-10 10:38:50 +0100

    * pre-run

--- packages/webpack-sw6-config/src/index.js
+++ packages/webpack-sw6-config/src/index.js
@@ -1,9 +1,13 @@
+const watcher = require('@pixolith/webpack-watcher');
 const dev = require('./webpack.config.dev');
 const production = require('./webpack.config.production');
 const storefront = require('./webpack.config.storefront');
 const administration = require('./webpack.config.administration');
 const isProd = process.env.NODE_ENV === 'production';
 const merge = require('webpack-merge');
+
+watcher.clean();
+watcher.run();
 
 module.exports = {
     storefront: merge(dev, storefront, isProd ? production : {}),
